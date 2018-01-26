d34e9614f863c2e6e53eb2aac85517cae9dd7eff
j.bultez <jordan.bultez@gmail.com>
2018-01-26 10:46:38 +0100

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'jordan-locabnb.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = {:host => 'locabnb.herokuapp.com', :protocol => 'https'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
